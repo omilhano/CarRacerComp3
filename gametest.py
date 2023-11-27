02fc6d5324b5c98634d5823b30da7c011e423103
Alexandre Francisco <alexmvfrancisco@gmail.com>
2023-11-27 21:57:56 +0000

    score system (kinda) added images

--- gametest.py
+++ gametest.py
@@ -1,4 +1,4 @@
-import pygame, random
+import pygame
 from car_test import Car
 
 
@@ -10,15 +10,20 @@ def car_racing():
     res = (1282, 800)
     screen = pygame.display.set_mode(res)
     pygame.display.set_caption("Car Racing")
+    # to run everything
     carryOn = True
+    # to have game state
+    game_active = True
     clock = pygame.time.Clock()
     playerCar = Car(130, 680, 70)
     player_group = pygame.sprite.Group()
     player_group.add(playerCar)
+    corbelfont = pygame.font.SysFont('Corbel', 50)  # Select font and size
+    start_time = 0
 
     def pause():
+        game_active = False
         loop = True
-        corbelfont = pygame.font.SysFont('Corbel', 50)  # Select font and size
         pausetext = corbelfont.render("Game is Paused", True, (100, 25, 225))
         spacebartext = corbelfont.render("Press Spacebar to continue", True, (100, 25, 225))
         screen.blit(pausetext, [200, 200])
@@ -33,6 +38,12 @@ def car_racing():
                         loop = False
             pygame.display.update()
             clock.tick(60)
+
+    def display_score():
+        current_time = int(pygame.time.get_ticks() / 1000) - start_time
+        score_surface = corbelfont.render(f" Score:{current_time}", False, (197, 136, 215))
+        score_rect = score_surface.get_rect(center=(400, 50))
+        screen.blit(score_surface, score_rect)
 
     speed = 1  # TODO what is this
 
@@ -56,13 +67,21 @@ def car_racing():
         if keys[pygame.K_ESCAPE]:
             pause()
 
-        screen.blit(bg, (0, 0))
-        print(playerCar.rect.x)
-        print(playerCar.rect.y)
-        player_group.draw(screen)
-        # Refresh Screen
-        pygame.display.flip()
+        if game_active:
+
+            screen.blit(bg, (0, 0))
+            display_score()
+            # test position
+            # print(playerCar.rect.x)
+            # print(playerCar.rect.y)
+            player_group.draw(screen)
+        else:
+            # TODO GAME OVER MENU
+            start_time = int(pygame.time.get_ticks() / 1000)
+            screen.fill((0, 0, 0))
+
         # Number of frames per second e.g. 60
         clock.tick(60)
-
+        # Refresh Screen
+        pygame.display.flip()
     pygame.quit()
