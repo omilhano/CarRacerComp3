eb9c824809cc5cce9f0ba6d58f9fe4994d0f2c94
Alexandre Francisco <alexmvfrancisco@gmail.com>
2023-11-27 23:17:34 +0000

    displaying hp and added images eli did

--- healthbar.py
+++ healthbar.py
@@ -1,6 +1,8 @@
 import pygame
 
+
 class Healthbar():
+
     def __init__(self, x, y, width, height, max_hp):
         self.x = x
         self.y = y
@@ -10,7 +12,13 @@ class Healthbar():
         self.max_hp = max_hp
 
     def draw(self, surface):
+        # font
+        corbelfont = pygame.font.SysFont('Corbel', 40)  # Select font and size
         # calculate the ratio between current and max
         ratio = self.hp / self.max_hp
-        pygame.draw.rect(surface, "red", (self.x, self.y, self.width, self.height))
-        pygame.draw.rect(surface, "green", (self.x, self.y, self.width * ratio, self.height))
+        pygame.draw.rect(surface, (217,19,68), (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(surface, (88,205,132), (self.x, self.y, self.width * ratio, self.height))
+        # display current hp
+        hp_text = corbelfont.render(f"HP: {int(self.hp)}", True, (20, 46, 77))
+        hp_rect = hp_text.get_rect(topleft=(10, 7))
+        surface.blit(hp_text, hp_rect)
